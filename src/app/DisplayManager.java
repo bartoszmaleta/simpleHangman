9ddd102c473f8fb8e451bd19ea187aff068a0138
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-15 23:49:31 +0100

    Add maniMenu()

---
+++ src/app/DisplayManager.java
@@ -0,0 +1,9 @@
+public class DisplayManager{
+    public static void mainMenu(){
+        System.out.println("HANGMAN GAME");
+        System.out.println("\n[1]-start game");
+        System.out.println("[2]-credits");
+        System.out.println("[3]-exit game");
+        System.out.println("\nChoose one option: ");
+    }
+}
